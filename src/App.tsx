bb18d5b371156e0a6d79ae360bda0f42209560ec
cyanidefire <lucy16813d@gmail.com>
2025-10-07 11:43:35 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from 'C:\Users\b00844308\Demanu_Fabric\public\vite.svg'
 import './App.css'
 
 function App() {
